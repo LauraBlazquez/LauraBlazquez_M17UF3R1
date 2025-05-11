daefe17c9aeab26027057e435bf9515908a7ff1b
Tottii05 <laura.blazquez.7e7@itb.cat>
2025-05-11 18:23:51 +0200

    player animator al completo, colision de camera

--- Assets/Characters/Player/Player.cs
+++ Assets/Characters/Player/Player.cs
@@ -1,25 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour/*, IDamageable*/
+public class Player : MonoBehaviour
 {
     public float maxHealth = 100f;
-    //private float currentHealth;
     public float moveSpeed = 3.5f;
+    public float runSpeed = 14f;
+    public float jumpForce = 5f;
     private PlayerControlls controls;
     private Vector2 movementInput;
     private Rigidbody rb;
+    public Animator animator;
+    private bool isRunning;
+    public bool isGrounded;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         controls = new PlayerControlls();
-    }
-
-    private void Start()
-    {
-        //currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -27,21 +26,56 @@ public class Player : MonoBehaviour/*, IDamageable*/
         controls.Gameplay.Enable();
         controls.Gameplay.Walk.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
         controls.Gameplay.Walk.canceled += ctx => movementInput = Vector2.zero;
+        controls.Gameplay.Run.performed += ctx => isRunning = true;
+        controls.Gameplay.Run.canceled += ctx => isRunning = false;
+        controls.Gameplay.Shoot.performed += ctx => animator.SetTrigger("Shoot");
+        controls.Gameplay.Jump.performed += ctx => Jump();
+        controls.Gameplay.Crouch.performed += ctx => animator.SetTrigger("Crouch");
+        controls.Gameplay.Crouch.canceled += ctx => animator.SetTrigger("Stand");
     }
 
     private void OnDisable()
     {
         controls.Gameplay.Walk.performed -= ctx => movementInput = ctx.ReadValue<Vector2>();
         controls.Gameplay.Walk.canceled -= ctx => movementInput = Vector2.zero;
+        controls.Gameplay.Run.performed -= ctx => isRunning = true;
+        controls.Gameplay.Run.canceled -= ctx => isRunning = false;
+        controls.Gameplay.Shoot.performed -= ctx => animator.SetTrigger("Shoot");
+        controls.Gameplay.Jump.performed -= ctx => Jump();
+        controls.Gameplay.Crouch.performed -= ctx => animator.SetTrigger("Crouch");
+        controls.Gameplay.Crouch.canceled -= ctx => animator.SetTrigger("Stand");
         controls.Gameplay.Disable();
     }
 
     private void FixedUpdate()
     {
         Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
-        rb.velocity = move * moveSpeed;
+        float currentSpeed = isRunning ? runSpeed : moveSpeed;
+        rb.velocity = new Vector3(move.x * currentSpeed, rb.velocity.y, move.z * currentSpeed);
+
+        isGrounded = Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, 1.1f);
+
+        if (movementInput != Vector2.zero)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", isRunning);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+        }
     }
 
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animator.SetTrigger("Jump");
+            isGrounded = false;
+        }
+    }
 
     //public void TakeDamage(float damage)
     //{
@@ -60,4 +94,3 @@ public class Player : MonoBehaviour/*, IDamageable*/
     //    Destroy(gameObject);
     //}
 }
-
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour
 {
@@ -9,13 +11,16 @@ public class CameraController : MonoBehaviour
     public float mouseSensitivity = 0.3f;
     public float minY = -90f;
     public float maxY = 90f;
+    public Player player;
+    public float danceAnimationDuration = 3f;
 
     private PlayerControlls controls;
     private bool isAiming;
     private Vector2 lookInput;
     private float xRotation = 0f;
-
     private Transform currentTarget;
+    private bool isDancing;
+    public List<GameObject> itemsToHide = new List<GameObject>();
 
     private void Awake()
     {
@@ -24,17 +29,23 @@ public class CameraController : MonoBehaviour
         controls.Camera.Look.canceled += ctx => lookInput = Vector2.zero;
         controls.Gameplay.Aim.performed += ctx => isAiming = true;
         controls.Gameplay.Aim.canceled += ctx => isAiming = false;
+        controls.Gameplay.Dance.performed += ctx => Dance();
     }
-
 
     private void OnEnable()
     {
         controls.Gameplay.Enable();
         controls.Camera.Enable();
+        controls.Gameplay.Dance.performed -= ctx => Dance();
     }
 
     private void OnDisable()
     {
+        controls.Camera.Look.performed -= ctx => lookInput = ctx.ReadValue<Vector2>();
+        controls.Camera.Look.canceled -= ctx => lookInput = Vector2.zero;
+        controls.Gameplay.Aim.performed -= ctx => isAiming = true;
+        controls.Gameplay.Aim.canceled -= ctx => isAiming = false;
+        controls.Gameplay.Dance.performed -= ctx => Dance();
         controls.Disable();
     }
 
@@ -47,18 +58,52 @@ public class CameraController : MonoBehaviour
 
     private void LateUpdate()
     {
-        currentTarget = isAiming ? firstPersonPivot : thirdPersonPivot;
-        transform.position = Vector3.Lerp(transform.position, currentTarget.position, Time.deltaTime * cameraSpeed);
-        transform.rotation = Quaternion.Lerp(transform.rotation, currentTarget.rotation, Time.deltaTime * cameraSpeed);
+        if (!isDancing)
+        {
+            currentTarget = isAiming ? firstPersonPivot : thirdPersonPivot;
+            transform.position = Vector3.Lerp(transform.position, currentTarget.position, Time.deltaTime * cameraSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, currentTarget.rotation, Time.deltaTime * cameraSpeed);
+        }
     }
 
     private void Update()
     {
-        float mouseX = lookInput.x * mouseSensitivity;
-        float mouseY = lookInput.y * mouseSensitivity;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, minY, maxY);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        if (!isDancing)
+        {
+            float mouseX = lookInput.x * mouseSensitivity;
+            float mouseY = lookInput.y * mouseSensitivity;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, minY, maxY);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+
+    public void Dance()
+    {
+        if (!isDancing)
+        {
+            player.GetComponent<Animator>().SetTrigger("Dance");
+            foreach (GameObject item in itemsToHide)
+            {
+                item.SetActive(false);
+            }
+            StartCoroutine(DanceRoutine());
+        }
+    }
+
+    private IEnumerator DanceRoutine()
+    {
+        isDancing = true;
+        Vector3 frontPosition = playerBody.position + playerBody.forward * 9f + Vector3.up * 4f;
+        Quaternion lookAtPlayer = Quaternion.LookRotation(playerBody.position + Vector3.up * 4f - frontPosition);
+        transform.position = frontPosition;
+        transform.rotation = lookAtPlayer;
+        yield return new WaitForSeconds(danceAnimationDuration);
+        isDancing = false;
+        foreach (GameObject item in itemsToHide)
+        {
+            item.SetActive(true);
+        }
     }
 }
